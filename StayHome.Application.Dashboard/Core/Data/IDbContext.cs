44c384b45c1166424800d30c87f77fad01483463
Joudy <joudy.6.dabbit@gmail.com>
2023-05-04 18:20:31 +0300

    upgrade

--- StayHome.Application.Dashboard/Core/Data/IDbContext.cs
+++ StayHome.Application.Dashboard/Core/Data/IDbContext.cs
@@ -1,4 +1,4 @@
-using Neptunee.BaseCleanArchitecture.Database;
+using Neptunee.BaseCleanArchitecture.BaseDbContexts.Interfaces;
 
 namespace Domain.Interfaces.Data;
 
--- StayHome.Persistence/Context/StayHomeDbContext.cs
+++ StayHome.Persistence/Context/StayHomeDbContext.cs
@@ -4,13 +4,14 @@ using Domain.Interfaces.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Neptunee.BaseCleanArchitecture.BaseDbContexts;
+using Neptunee.BaseCleanArchitecture.BaseDbContexts.Interfaces;
 using Neptunee.BaseCleanArchitecture.Clock;
-using Neptunee.BaseCleanArchitecture.Database;
 using Neptunee.BaseCleanArchitecture.Dispatchers.DomainEventDispatcher;
 
 namespace Presentation.Context;
 
-public class StayHomeDbContext : BaseIdentityDbContext<Guid,User>, IDbContext
+public class StayHomeDbContext : BaseIdentityDbContext<Guid,User>, IDbContext, IBaseDbContext<Guid>
 {
     public StayHomeDbContext(DbContextOptions options, IClock clock, IDomainEventDispatcher domainEventDispatcher) : base(options, clock, domainEventDispatcher)
     {
