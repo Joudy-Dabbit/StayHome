7a9c0c894d1b62c20bcf50d65bd93acd3c99b153
Joudy <joudy.6.dabbit@gmail.com>
2023-05-05 14:00:01 +0300

    jwt

--- StayHome.Domain/ConstValues.cs
+++ StayHome.Domain/ConstValues.cs
@@ -2,10 +2,26 @@ namespace Domain;
 
 public class ConstValues
 {
+    public const int ExpireAccessTokenMinute = 2;
+    public static DateTime AccessExpireDateTime = DateTime.UtcNow.AddDays(ExpireAccessTokenMinute);
+
     public class UserType
     {
         public const string Driver = "Driver";
         public const string Employee = "Employee";
         public const string Customer = "Customer";
+    }
+    
+    public class AppRoles
+    {
+        public const string Admin = "Admin";
+        public const string Shop = "Shop";
+        public const string Customer = "Customer";
+        public const string Driver = "Driver";
+    }
+
+    public static class AppClaims
+    {
+        public const string Type = "Type";
     }
 }
--- StayHome.Persistence/Repositories/UserRepository.cs
+++ StayHome.Persistence/Repositories/UserRepository.cs
@@ -1,4 +1,5 @@
 using System.IdentityModel.Tokens.Jwt;
+using System.Net;
 using System.Security.Claims;
 using System.Text;
 using Application.Dashboard.Core.Jwt;
@@ -7,11 +8,15 @@ using Domain.Entities;
 using Domain.Entities.Security;
 using Domain.Enum;
 using Domain.Repositories;
+using EasyRefreshToken.Service;
 using Microsoft.AspNetCore.Identity;
 using StayHome.Infrastructure.Jwt.Claims;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using Neptunee.BaseCleanArchitecture.OResponse;
 using Neptunee.BaseCleanArchitecture.Repository;
+using StayHome.Contracts.Shared;
 using StayHome.Presentation.Context;
 using StayHome.Presentation.Repositories;
 
@@ -19,13 +24,17 @@ namespace StayHome.Presentation.Repositories;
 
 public class UserRepository : StayHomeRepository, IUserRepository
 {
-   // private readonly IJwtService _jwt;
+    private readonly IJwtService _jwt;
     private readonly UserManager<User> _userManager;
+    private readonly IConfiguration _configuration;
+    private readonly ITokenService<Guid> tokenService;
 
-    public UserRepository(StayHomeDbContext context /* ,IJwtService jwt*/, UserManager<User> userManager) : base(context)
+    public UserRepository(StayHomeDbContext context ,IJwtService jwt, UserManager<User> userManager, IConfiguration configuration, ITokenService<Guid> tokenService) : base(context)
     {
-       // _jwt = jwt;
+        _jwt = jwt;
         _userManager = userManager;
+        _configuration = configuration;
+        this.tokenService = tokenService;
     }
     public async Task<IdentityResult> AddWithRole(User user, StayHomeRoles role, string? password = null)
     {
@@ -47,31 +56,74 @@ public class UserRepository : StayHomeRepository, IUserRepository
         identityResult = await _userManager.AddToRoleAsync(user, role.ToString());
         return identityResult;
     }
-    // public async Task<string> GetEmployeeAccessToken(Employee employee)
-    // {
-    //     var claims = await _userRepository.GetAllClaimsAsync(employee.Id);
-    //     AddUserTypeClaim(claims, ConstValues.UserType.Employee);
-    //     return _jwt.GenerateJwtToken(claims);
-    // }
-
-    // public async Task<List<Claim>> GetAllClaimsAsync(Guid userId)
-    // {
-    //     var claims = await _userRepository.Query<User>().Where(u => u.Id == userId)
-    //         .Select(u => new
-    //         {
-    //             Roles = u.UserRoles.Select(ur => new Claim(StayHomeClaimTypes.Role, ur.Role.Name)),
-    //             UserClaims = u.UserClaims.Select(uc => uc.ToClaim())
-    //         }).FirstAsync();
-    //     
-    //     return new List<Claim>
-    //     {
-    //         new(StayHomeClaimTypes.NameIdentifier, userId.ToString()),
-    //         new(StayHomeClaimTypes.GenerateDate, DateTime.Now.ToLocalTime().ToString("G")),
-    //     }.Union(claims.Roles).Union(claims.UserClaims).ToList();
-    // }
-    public static List<Claim> AddUserTypeClaim(List<Claim> claims, string userType)
+    public string GenerateAccessToken(User user, IList<string> roles, DateTime expierDate)
     {
-        claims.Add(new Claim("user-type", userType));
-        return claims;
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.UserName.ToString()),
+            new Claim(ConstValues.AppClaims.Type, user.GetType().ToString()),
+        };
+
+        foreach (var role in roles)
+        {
+            claims.Add(new Claim(ClaimTypes.Role, role));
+        }
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+        var token = new JwtSecurityToken(_configuration["Jwt:Issuer"],
+            _configuration["Jwt:Issuer"],
+            claims,
+            expires: expierDate,
+            signingCredentials: creds);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+    
+    public async Task<OperationResponse<TokenDto>> RefreshToken(string accessToken, string refreshToken)
+    {
+        var principal = GetPrincipalFromExpiredToken(accessToken);
+        var username = principal.Identity.Name; //this is mapped to the Name claim by default
+
+        var user = Context.Users.SingleOrDefault(u => u.UserName == username);
+        if (user == null)
+            return OperationResponse.With(HttpStatusCode.NotFound, "NotExist").ToResponse<TokenDto>();;
+
+        var tokenResult = await tokenService.OnAccessTokenExpired(user.Id, refreshToken);
+
+        if (!tokenResult.IsSucceded)
+            return OperationResponse.WithBadRequest(tokenResult.ErrorMessage).ToResponse<TokenDto>();
+
+        var roles = await _userManager.GetRolesAsync(user);
+        var newAccessToken = GenerateAccessToken(user, roles, ConstValues.AccessExpireDateTime);
+
+        await Context.SaveChangesAsync();
+        return OperationResponse.WithOk().ToResponse(new TokenDto
+        {
+            AccessToken = newAccessToken,
+            RefreshToken = tokenResult.Token,
+        });
+    }
+
+    public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+    {
+        var tokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false, //you might want to validate the audience and issuer depending on your use case
+            ValidateIssuer = false,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"])),
+            ValidateLifetime = false //here we are saying that we don't care about the token's expiration date
+        };
+        var tokenHandler = new JwtSecurityTokenHandler();
+        SecurityToken securityToken;
+        var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out securityToken);
+        var jwtSecurityToken = securityToken as JwtSecurityToken;
+        if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256Signature, StringComparison.InvariantCultureIgnoreCase))
+            throw new SecurityTokenException("Invalid token");
+        return principal;
     }
 }
