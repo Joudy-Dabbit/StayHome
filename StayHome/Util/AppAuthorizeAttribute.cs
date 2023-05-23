bee26ee05f5ee07732fc98ef4f17c6f45504f579
Joudy <joudy.6.dabbit@gmail.com>
2023-05-23 13:08:30 +0300

    AppAuthorizeAttribute

--- StayHome/Util/AppAuthorizeAttribute.cs
+++ StayHome/Util/AppAuthorizeAttribute.cs
@@ -8,6 +8,6 @@ public class AppAuthorizeAttribute : AuthorizeAttribute
     public AppAuthorizeAttribute(params StayHomeRoles[] roles)
     {
         Roles = string.Join(",", roles.Select(x => x.ToString()));
-        AuthenticationSchemes = "Bearer";
+        AuthenticationSchemes = "Bearer"; 
     }
 }
