dd692eb846799dcb2dca62bfe8a8997b5c28a0e8
Joudy <joudy.6.dabbit@gmail.com>
2023-06-23 21:59:01 +0300

    CustomerGender

---
+++ StayHome.Persistence/Migrations/20230623185653_CustomerGender.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StayHome.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class CustomerGender : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "AspNetUsers");
+        }
+    }
+}
