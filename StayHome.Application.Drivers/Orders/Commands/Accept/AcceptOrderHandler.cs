81e505650e03792edcef030251af5c33ff8141ce
Joudy <joudy.6.dabbit@gmail.com>
2023-08-21 23:48:40 +0300

    Driver => order

--- StayHome.Application.Drivers/Orders/Commands/Accept/AcceptOrderHandler.cs
+++ StayHome.Application.Drivers/Orders/Commands/Accept/AcceptOrderHandler.cs
@@ -4,6 +4,7 @@ using Domain.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Neptunee.BaseCleanArchitecture.OResponse;
 using Neptunee.BaseCleanArchitecture.Requests;
+using StayHome.Application.Dashboard.Core.Abstractions.Http;
 
 namespace StayHome.Application.Drivers.Orders;
 
@@ -11,21 +12,28 @@ public class AcceptOrderHandler: IRequestHandler<AcceptOrderCommand.Request,
     OperationResponse>
 {
     private readonly IStayHomeRepository _repository;
+    private readonly IHttpService _httpService;
 
-    public AcceptOrderHandler(IStayHomeRepository repository)
+    public AcceptOrderHandler(IStayHomeRepository repository, IHttpService httpService)
     {
         _repository = repository;
+        _httpService = httpService;
     }
 
     public async Task<OperationResponse> HandleAsync(AcceptOrderCommand.Request request,
         CancellationToken cancellationToken = new())
     {
-      var  order = await _repository.TrackingQuery<Order>()
+        if(_repository.Query<Order>()
+               .Where(o => o.DriverId == _httpService.CurrentUserId!.Value)
+               .Any(o => o.Stages.OrderByDescending(os => os.DateTime).First().CurrentStage == OrderStages.OnWay))
+            return OperationResponse.WithBadRequest("You already have an order being delivered");
+        
+        var  order = await _repository.TrackingQuery<Order>()
           .Where(o => o.Id == request.Id).FirstAsync(cancellationToken);
       
-      order.AddStage(OrderStages.OnWay);
-      await _repository.UnitOfWork.SaveChangesAsync(cancellationToken);
-      
-      return OperationResponse.WithOk();
+        order.AddStage(OrderStages.OnWay);
+        await _repository.UnitOfWork.SaveChangesAsync(cancellationToken);
+        
+        return OperationResponse.WithOk();
     }
 }
